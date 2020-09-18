3bfa10c1de9c543dc160db651c633f231a20fdef
Guram Khubulava <guram.khubulava@wandio.com>
2020-09-18 11:48:00 +0400

    upload

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Movie Catalogue</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
